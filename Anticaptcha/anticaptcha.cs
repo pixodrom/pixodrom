f0236092d7b6e3e6bd8a1dc6be293225a47c12cb
Sergio <shirvlad@gmail.com>
2014-12-07 00:26:11 +0100

    initial project version

---
+++ Anticaptcha/anticaptcha.cs
@@ -0,0 +1,432 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Threading;
+
+namespace Anticaptcha
+{
+    public class anticaptcha
+    {
+        public string soft_id = "e5c979564";
+        public string key;
+        public int phrase = 0;
+        public int is_russian = 1;
+        public int numeric = 0;
+        public int calc = 0;
+        public int regsense = 0;
+        public int min_len = 0;
+        public int max_len = 0;
+        public string label;
+
+        public anticaptcha(string key)
+        {
+            this.key = key;
+        }
+
+        #region GET/POST
+        public static string GetPage(string url)
+        {
+            var request = (HttpWebRequest)HttpWebRequest.Create(url);
+            using (var response = (HttpWebResponse)request.GetResponse())
+            {
+                using (var responseReader = new StreamReader(response.GetResponseStream(), Encoding.GetEncoding("utf-8")))
+                {
+                    return responseReader.ReadToEnd();
+                }
+            }
+        }
+
+        public static string PostPage(string url, string postData)
+        {
+            byte[] postContentData = Encoding.GetEncoding("windows-1251").GetBytes(postData);
+            if (postContentData.Length > 0)
+            {
+                var req = (HttpWebRequest)WebRequest.Create(url);
+                req.Proxy = new WebProxy();
+                req.ContentType = "application/x-www-form-urlencoded";
+                req.Method = "POST";
+
+                using (Stream stream = req.GetRequestStream())
+                {
+                    stream.Write(postContentData, 0, postContentData.Length);
+                    stream.Close();
+                }
+
+                using (var response = (HttpWebResponse)req.GetResponse())
+                {
+                    using (var responseReader = new StreamReader(response.GetResponseStream(), Encoding.GetEncoding("utf-8")))
+                    {
+                        return responseReader.ReadToEnd();
+                    }
+                }
+            }
+            return null;
+        }
+        #endregion
+
+        #region Data Convertation
+        public static byte[] ImageToBytes(Image imageIn, System.Drawing.Imaging.ImageFormat imgFormat)
+        {
+            using (var ms = new MemoryStream())
+            {
+                imageIn.Save(ms, imgFormat);
+                return ms.ToArray();
+            }
+        }
+
+        public static Image Base64ToImage(string base64String)
+        {
+            byte[] imageBytes = Convert.FromBase64String(base64String);
+            using (var ms = new MemoryStream(imageBytes, 0, imageBytes.Length))
+            {
+                ms.Write(imageBytes, 0, imageBytes.Length);
+                return System.Drawing.Image.FromStream(ms, true);
+            }
+        }
+
+        public static string ImageToBase64(System.Drawing.Image image, System.Drawing.Imaging.ImageFormat format)
+        {
+            using (var ms = new MemoryStream())
+            {
+                image.Save(ms, format);
+                byte[] imageBytes = ms.ToArray();
+
+                return Convert.ToBase64String(imageBytes);
+            }
+        }
+
+        public static byte[] Base64ToBytes(string base64String)
+        {
+            byte[] imageBytes = Convert.FromBase64String(base64String);
+            return imageBytes;
+        }
+
+        public static System.Drawing.Image BytesToImage(byte[] byteArrayIn)
+        {
+            var ms = new MemoryStream(byteArrayIn);
+            return System.Drawing.Image.FromStream(ms);
+        }
+        #endregion
+
+        #region Image Format Methods
+        public static string ToMIME(string extension)
+        {
+            switch (extension.ToLower().Replace(".", ""))
+            {
+                case "jpg": return "image/jpg";
+                case "jpeg": return "image/jpeg";
+                case "gif": return "image/gif";
+                case "png": return "image/png";
+                case "bmp": return "image/bmp";
+                case "tiff": return "image/tiff";
+                default: return extension;
+            }
+        }
+
+        public static ImageFormat DetectImageFormat(string fmt)
+        {
+            switch (fmt.ToLower().Replace(".", ""))
+            {
+                case "image/jpg": return ImageFormat.Jpeg;
+                case "image/jpeg": return ImageFormat.Jpeg;
+                case "image/gif": return ImageFormat.Gif;
+                case "image/png": return ImageFormat.Png;
+                case "image/bmp": return ImageFormat.Bmp;
+                case "image/tiff": return ImageFormat.Tiff;
+                case "jpg": return ImageFormat.Jpeg;
+                case "jpeg": return ImageFormat.Jpeg;
+                case "gif": return ImageFormat.Gif;
+                case "png": return ImageFormat.Png;
+                case "bmp": return ImageFormat.Bmp;
+                case "tiff": return ImageFormat.Tiff;
+                default: return null;
+            }
+        }
+
+        public static ImageFormat DetectImageFormat(Image image)
+        {
+            if (ImageFormat.Jpeg.Equals(image.RawFormat))
+            {
+                return ImageFormat.Jpeg;
+            }
+            else if (ImageFormat.Png.Equals(image.RawFormat))
+            {
+                return ImageFormat.Png;
+            }
+            else if (ImageFormat.Gif.Equals(image.RawFormat))
+            {
+                return ImageFormat.Gif;
+            }
+            else if (ImageFormat.Bmp.Equals(image.RawFormat))
+            {
+                return ImageFormat.Bmp;
+            }
+            else if (ImageFormat.Tiff.Equals(image.RawFormat))
+            {
+                return ImageFormat.Tiff;
+            }
+            else
+                return ImageFormat.Jpeg;
+        }
+
+        public string GetExtension(byte[] imageBytes)
+        {
+            using (var ms = new MemoryStream(imageBytes))
+            {
+                System.Drawing.Image image = System.Drawing.Image.FromStream(ms);
+                ImageFormat fmt = DetectImageFormat(image);
+                return fmt.ToString().ToLower().Replace("jpeg", "jpg");
+            }
+        }
+        #endregion
+
+        #region Image Downloaders
+        public byte[] DownloadImage(string imgURL, out ImageFormat imageFormat)
+        {
+            imageFormat = null;
+
+            var req = (HttpWebRequest)HttpWebRequest.Create(imgURL);
+            req.UserAgent =
+                "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; .NET CLR 1.1.4322; .NET CLR 2.0.50727; .NET CLR 3.0.04506.590; .NET CLR 3.5.20706)";
+            req.Accept = "*/*";
+            req.Headers.Add("Accept-Language", "ru");
+            req.Proxy = new WebProxy();
+            req.KeepAlive = true;
+            req.AllowAutoRedirect = false;
+            req.Method = "GET";
+
+            using (var resp = (HttpWebResponse)req.GetResponse())
+            {
+                using (Stream stream = resp.GetResponseStream())
+                {
+                    if (stream != null)
+                    {
+                        Image image = Image.FromStream(stream);
+                        imageFormat = DetectImageFormat(image);
+                        return ImageToBytes(image, imageFormat);
+                    }
+                }
+            }
+            return new byte[] { };
+        }
+
+        public byte[] DownloadImage(string imgURL)
+        {
+            var req = (HttpWebRequest)WebRequest.Create(imgURL);
+            req.Proxy = new WebProxy();
+            req.AllowAutoRedirect = true;
+            req.Method = "GET";
+            req.UserAgent =
+                "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; .NET CLR 1.1.4322; .NET CLR 2.0.50727; .NET CLR 3.0.04506.590; .NET CLR 3.5.20706)";
+
+            using (var resp = (HttpWebResponse)req.GetResponse())
+            {
+                using (var stream = resp.GetResponseStream())
+                {
+                    using (var ms = new MemoryStream())
+                    {
+                        if (stream != null) stream.CopyTo(ms);
+                        return ms.ToArray();
+                    }
+                }
+            }
+        }
+        #endregion
+
+        #region Pixodrom Image Uploaders
+        public string Upload(byte[] imgbytes)
+        {
+            string sBoundary = DateTime.Now.Ticks.ToString("x");
+
+            var req = (HttpWebRequest)HttpWebRequest.Create("http://pixodrom.com/in.php");
+            req.UserAgent = "Mozilla";
+            req.Accept = "*/*";
+            req.Headers.Add("Accept-Language", "ru");
+            req.Proxy = new WebProxy();
+            req.KeepAlive = true;
+            req.AllowAutoRedirect = false;
+            req.Method = "POST";
+            req.ContentType = String.Format("multipart/form-data; boundary={0}", sBoundary);
+
+            var sPostMultiString = new StringBuilder();
+            sPostMultiString.Append(PostData.MultiFormData("method", "post", sBoundary));
+            sPostMultiString.Append(PostData.MultiFormData("key", key, sBoundary));
+            //sPostMultiString.Append(PostData.MultiFormData("file", filename, sBoundary));
+            sPostMultiString.Append(PostData.MultiFormData("calc", calc.ToString(), sBoundary));
+            sPostMultiString.Append(PostData.MultiFormData("numeric", numeric.ToString(), sBoundary));
+            sPostMultiString.Append(PostData.MultiFormData("phrase", phrase.ToString(), sBoundary));
+            sPostMultiString.Append(PostData.MultiFormData("minlen", min_len.ToString(), sBoundary));
+            sPostMultiString.Append(PostData.MultiFormData("maxlen", max_len.ToString(), sBoundary));
+            sPostMultiString.Append(PostData.MultiFormData("is_russian", is_russian.ToString(), sBoundary));
+            sPostMultiString.Append(PostData.MultiFormData("label", label, sBoundary));
+            sPostMultiString.Append(PostData.MultiFormData("soft_id", soft_id, sBoundary));
+
+            string sFileContent = Encoding.Default.GetString(imgbytes);
+
+            sPostMultiString.Append(PostData.MultiFormDataFile("file", sFileContent, "captcha", "", sBoundary));
+            sPostMultiString.Append(String.Format("--{0}--\r\n\r\n", sBoundary));
+
+            byte[] byteArray = Encoding.Default.GetBytes(sPostMultiString.ToString());
+            req.ContentLength = byteArray.Length;
+            req.GetRequestStream().Write(byteArray, 0, byteArray.Length);
+
+            using (var response = (HttpWebResponse)req.GetResponse())
+            {
+                using (var reader = new StreamReader(response.GetResponseStream(), Encoding.Default))
+                {
+                    return reader.ReadToEnd();
+                }
+            }
+        }
+
+        public string UploadImage(Image image)
+        {
+            return Upload(ImageToBytes(image, DetectImageFormat(image)));
+        }
+
+        public string UploadFile(string filename)
+        {
+            return Upload(File.ReadAllBytes(filename));
+        }
+
+        public string UploadURL(string imgURL)
+        {
+            return Upload(DownloadImage(imgURL));
+        }
+
+        public string UploadBase64Bytes(byte[] imgbytes)
+        {
+            return UploadBase64(Convert.ToBase64String(imgbytes));
+        }
+
+        public string UploadBase64(string base64)
+        {
+            var sPostString = new StringBuilder();
+            sPostString.Append("method=base64");
+            sPostString.Append(String.Format("&key={0}", this.key));
+            sPostString.Append(String.Format("&is_russian={0}", is_russian));
+            sPostString.Append(String.Format("&phrase={0}", phrase));
+            sPostString.Append(String.Format("&regsense={0}", regsense));
+            sPostString.Append(String.Format("&numeric={0}", numeric));
+            sPostString.Append(String.Format("&calc={0}", calc));
+            sPostString.Append(String.Format("&min_len={0}", min_len));
+            sPostString.Append(String.Format("&max_len={0}", max_len));
+            sPostString.Append(String.Format("&soft_id={0}", soft_id));
+            sPostString.Append(String.Format("&body={0}", base64));
+
+            string postData = sPostString.ToString();
+
+            byte[] postContentData = Encoding.GetEncoding("windows-1251").GetBytes(postData);
+            if (postContentData.Length > 0)
+            {
+                var req = (HttpWebRequest)WebRequest.Create("http://pixodrom.com/in.php");
+                req.Proxy = new WebProxy();
+                req.ContentType = "application/x-www-form-urlencoded";
+                req.Method = "POST";
+
+                using (Stream stream = req.GetRequestStream())
+                {
+                    stream.Write(postContentData, 0, postContentData.Length);
+                    stream.Close();
+                }
+
+                using (var response = (HttpWebResponse)req.GetResponse())
+                {
+                    using (var responseReader = new StreamReader(response.GetResponseStream(), Encoding.GetEncoding("windows-1251")))
+                    {
+                        return responseReader.ReadToEnd();
+                    }
+                }
+            }
+            else return "ERROR_IMAGE_UPLOADING";
+        }
+
+        public string UploadImageBase64(Image image)
+        {
+            return UploadBase64Bytes(ImageToBytes(image, DetectImageFormat(image)));
+        }
+
+        public string UploadFileBase64(string filename)
+        {
+            return UploadBase64Bytes(File.ReadAllBytes(filename));
+        }
+
+        public string UploadURLBase64(string url)
+        {
+            return UploadBase64Bytes(DownloadImage(url));
+        }
+        #endregion
+
+        #region Pixodrom Methods
+        public string GetBalance()
+        {
+            return GetPage(String.Format("http://pixodrom.com/res.php?key={0}&action=getbalance", key));
+        }
+
+        public string GetResult(string capid)
+        {
+            return GetPage(String.Format("http://pixodrom.com/res.php?key={0}&action=get&id={1}", key, capid));
+        }
+
+        public string GetResults(string ids, bool isPost = false)
+        {
+            if (!isPost)
+                return GetPage(String.Format("http://pixodrom.com}/res.php?key={0}&action=get&ids={1}", key, ids));
+            else
+                return PostPage("http://pixodrom.com/res.php",
+                    String.Format("key={0}&action=get&ids={1}", key, ids));
+        }
+
+        public void ReportBad(string capid)
+        {
+            GetPage(String.Format("http://pixodrom.com/res.php?key={0}&action=reportbad&id={1}", key, capid));
+        }
+        #endregion
+    }
+
+    public class PostData
+    {
+        private readonly string _sMethod = String.Empty;
+        private readonly string _sAction = String.Empty;
+        private readonly string _sParam = String.Empty;
+
+        public string Method { get { return this._sMethod; } }
+        public string Action { get { return this._sAction; } }
+        public string Param { get { return this._sParam; } }
+
+        public PostData(string sPostString)
+        {
+            if (sPostString != null && sPostString.IndexOf("=", System.StringComparison.Ordinal) != -1)
+            {
+                this._sMethod = sPostString.Substring(0, sPostString.IndexOf("=", System.StringComparison.Ordinal));
+                this._sAction = sPostString.Substring(sPostString.IndexOf("=", System.StringComparison.Ordinal) + 1);
+                if (this._sAction.IndexOf("!", System.StringComparison.Ordinal) != -1)
+                {
+                    this._sAction = _sAction.Substring(0, this._sAction.IndexOf("!", System.StringComparison.Ordinal));
+                    this._sParam = sPostString.Substring(sPostString.IndexOf("!", System.StringComparison.Ordinal) + 1);
+                }
+            }
+        }
+
+        public static string MultiFormData(string key, string value, string boundary)
+        {
+            var output = new StringBuilder(String.Format("--{0}\r\n", boundary));
+            output.Append(String.Format("Content-Disposition: form-data; name=\"{0}\"\r\n\r\n", key));
+            output.Append(String.Format("{0}\r\n", value));
+            return output.ToString();
+        }
+
+        public static string MultiFormDataFile(string key, string value, string fileName, string fileType, string boundary)
+        {
+            var output = new StringBuilder(String.Format("--{0}\r\n", boundary));
+            output.Append(String.Format("Content-Disposition: form-data; name=\"{0}\"; filename=\"{1}\"\r\n", key, fileName));
+            output.Append(String.Format("Content-Type: {0} \r\n\r\n", fileType));
+            output.Append(String.Format("{0}\r\n", value));
+            return output.ToString();
+        }
+    }
+
+}
---
+++ TestConsole/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Net.Mime;
+using System.Text;
+using System.Threading;
+using Anticaptcha;
+
+namespace TestConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string key = "5f12031f91668a9022b788066739fd0e";
+            anticaptcha ac = new anticaptcha(key);
+
+            int uploadMode = 3;
+
+            //Step 1. Uploading file CAPTCHA "01.png" to anticaptcha service pixodrom.com
+            string res = "";
+            while (!res.StartsWith("OK|"))
+            {
+                switch (uploadMode)
+                {
+                    case 1:
+                        res = ac.UploadBase64("iVBORw0KGgoAAAANSUhEUgAAAMgAAABGCAMAAAC+PCsEAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAACHUExURWZVM5kAAJkAM5krAJkrM5lVAJlVM5lVZpmAZswAAMwAM8wrAMwrM8wrZsxVAMxVM8xVZsyAM8yAZsyAmcyqZsyqmcyqzMzVzMzV/8z/zMz///8AAP8AM/8rAP8rM/9VAP9VM/9VZv+AM/+AZv+Amf+qZv+qmf+qzP/Vmf/VzP/V////zP///4M2uHIAABAUSURBVGhD7ZqLdtvGEYZrA4YDYqtFINGOpVKVQgoQCL7/8/X7ZxYXyqAUJz5RT0+HIi6Lvcw/9wX1j9P/CP0fyH8bvSeQW/v7SfRuQG4FYrC/n0LvqBEgmEJ+DpT3AjLa1DGdnf6Cpb0PkMTwOQpr/dNQ3tPZheR4+s0AHR3BnzezdwByexpujeFJIX/BoiZ6H40AYZik3/tJsPo/r5K/HYgpAxQJS2L8q58WWvpRWgcyzfuzyfhEBcNx6Pkch6OfaJV92eM/p5TLGjEwP/nwTRfw22+bpm6Mnq7bbZu4d1/B+/Eiu/wB+rtNyxL6qW9zqAr6lFn+S2veoScOQJrRV+Df+iZaBXJmqDb1LKCzZ0uau5h0537LEbrW95BXVVXygUKV7d2cHOVIP2ZiFzRiNmAcyB99eoLmrdqcsaPubuWzajx6oz37evo6JCY42QO8QsLj2vsNXXQI92XQRb4devX47Qe5X9AaEFDYekIjDNwYx0Y6L+XmdDzd3p6+AY7r5+2NGfrRAmvqawzaJEdx3EsjVXXHt6zuq6p+ts7W7ciRjrbg9ytdojd9ZEJwOiYPnA3gVpkZvNyPaqNbk+d4r5G6DreWHYa+1zlJvDdFVGFX3lcoJQtNyiYXOHcJnH/nC9EakOnhYsBIukbuiiyJtLRLkyiqsPoYsrb3jmr+BoZOUaporrc9QISur4XjX3zNTYDUnoY06YRFUyBHqfm1r9NljUzsc+F/yXWctMJ44bxx3Z/6/nBfbeJoJwLWtU91DJlC1FUDxAErPD2aTZW7SoZV7TZV0YmvxaJi0VeYGl+jVSDiN/FsiH0mMyHI7MVXMufnYjAIfXvdxDyU9yGDK8pA+vdtG0Moq3BXbfhkoUUpoN7L1aWRHSrBzkKOcTnvtuTb9ALeBY3MvW4dkbiCLz+xlFk7wWkg3MimvjY1GAL2HhE1Jm8w+/YQBAOvluCrEKq8wdeOpzZUuwAyEXqhS3ZIscBWs+8SkrUntmbu5qvXfOSYwpa5tPNuU8Og6Lnvu+cOkbeY/nOOKsJOTIsKbIveX2o5NVFW4GKEcwxsq2ddJieXQgwhFlbGzsxTILSO1uRrtwuaWT+nNSALT9BENpXLXX7BTP2pyTH6PMssO+chDn2NtBPJUg5SyTONpbAJ3q+tddpUimnDEMt4fx/1oFQQlnG5ZxnN7JroLCZa0DTZ0iazGNuMLpjWjEU9pfN+23YwZ/MeCTrieyfLL0lqsRv2mQBAxhvQTv1wHbEa/BkK8WbADA+5ehOI+0j3lmwydkA1AavTev3Inc6CtIiRiQzojBa6AGTspaMsoW/rHMlfaw3cHAe21WXeMFlmN0MLMkS7E+uYS97iQNKSFLLb7CpCMi3HyH1ZI489ymyf7DnZxOaqNq2ihtMIZiRxcsa509S0AoRnyxHM3PdNQU2xCXVq2lYfxLWtL8Ug5GcVHeKIUIqv5DeuN0NKe6ga1S19B9cPWYcswLAfYm5j8Hw+0qwvYKtCK5yvtkEXNQLJvmRV3baO+OYOqxi4R2rN7BBOKKCRvxgRZ0PxD+ywDgmGjrlSyGmo8fysHk5tUVZN36Fa2SgykdfjJipsnFdZsVmyHy8enFaAiH/zkYQddRiHd7tNkKBYKmYyIUgP4CBQv7YeTEMoYh3rZosdtVnqQ9+QRZAwGQNi7k5SD/1W+iDcqaP6Nr6myI1rypG61Y3Ochp9F/b3mkYS9Y2He4h1EMLQFlgP6ezO3IJTGfJehqQYG6lEOhjVyFq6KHd3CPyqDBbKnlUuYlsnipQCr8ciA0a1K//9QX1Da8Ge0SMAI92cNTjRNCplDQiPx0GUtCfFWjMOMhg2ISAwg/+ao4LjXqd2IGeE31tlbj7q9/XUKJRhWYwkuJURpbTypbDV3koF1ol7UEjBTMUMUtZC1MbnQvAX6U2NAOlLzCOlkpy7kEJOjVvUXYqcZjqfCWVZtVf64I9RWt1rQ6VJwGjzESJFVpXF7njqciIZndljERykOMv+oegYrYSFHX2bJGqA0sHrMD9MoXkNiDuJujMN3Sk0GuQmX6jMYhAqPPmeSLqRnRddTwhKOc1G6tTiXjzdBSpJuqI6G0VdddQsNf3IkuZv4Q69ADI0Vqo4KeEtbteuEr2ikVGhcEVN6xqpVF/IrnchlqRDsznLaR/2A4oiejGA2kU5D1FgQJgLFcqY92RAVfWAJzALUZvZ+iL4Niv1yKwqSBXFRKOf+ytJO5zlyRUgjHiBl4D7nG0sQxBVetvePezFktbeyOMpFrs2I3qRKq5jQeRqu+FIwG0f1Dvx6A4Fr3Xb99QClGQsRXAzeK6YMmSqh2zhF3x8R25dRm/6iCSjHZNsi4UUgLdyyqZVlJKS4JKwFD8+9XWm8oNSXryqi5y+b4gCY9yjuxeJed0+xSweYOU4NEWGF5mjMBl6fT5Sz7BqguIh2Li2g24XGIwu+QhkPpU0TL2nnBDKT5jMlhXDk4oMFSMKwACryofByo4mKI7ShsBtjzF0+zpWuDaN8nvEAZgP8QE4lPt8+isaTGmSSQXA9NLOF3/J9HybXn2LLmhk3t6MU90gVtYqyWr4QhnbocORMX1Q3dkWKbS4dvYY750dSTZE8ymwsF0pgCmlyqdxeeHK4n7f1L8/UgUjEwsFwhMwPTYI+NmCpIa0K7XDi0pyDQhDXKN+0nDij+yFpfZ9/xlLeUTWKGljtiWmyYqUTtKVODXxGpQtrqK6ZujavTINTwJFJAnQuqApJRaDjrosximChCyP9kJi5HeSvfMkMlmPYNc1stBlmmDoMyvcq3zoyevKjGpi8ZFn5NupBLYk6a3GUE2aT1O0e6QuZuUneuhi0OYda7sLd0ooigaaDSi1vXhx3o2lia8ZzUhvODsjrWTA1FwjGFCn4lxhVuWSxX75siLXU/fJOEMxnmRQDeGMHRVT9R3hwYhHBkU6oMopMSw1yVCVOQ2lBJEX14nzJM1JK+fNRmtA9JzvmYWelCW0VPbUwn5UIiBukkyMI44sXfR7sGW/tnh8Ge7hVravalHvUupxExMfw6cPDJF6FKRCyAtqB3t2j30R5gFT4vT5YdaGXfH1TaFjUFOiFSAAmDpwgUYYNRytukV2SiApk5HKtLgd5KlsnvaEnI6gqwQKNAn2CiSCUZq2aHp8HkFRToZ9u2fb37YH+UqKwnqkVfIivepbcDxeJzk7otdNy/okLWLh2Jb4olqiJPdpbkxJo0KEb2jrz+SO43BInivKogaZ46jboyJuHT9IxXeUZ87Z8OSZExtFTxr5QGRknZHV2UNmmpvedHaRTdV7WmMFOFQ9RCii8ONOTb60tn69RVwST/J4J1iWoVl34IL1GmMSlIC7meL75hMjcJqUM8tICaq5DKjDSaAWLUk/lzTikTd1skLU9nYeVtnimYsQUzEzilZXS/lQZk0SL6T6nA+eQmUjJMBgk0nPyKaLz0FuUe4Y4zyRbLKNsGouGV2VXSf7mUWb8uT3ylkBMhcEeqch39I6w8mKKwWiMly1ppF+6LSmCVpSZKtOFjuqxAJkK4emspwsjF4CJH3CypFtuzYH2t5rhd/0TsOVAVppmBjgpYH4cU+1KydxOUltXSPfwzXCSdgUmu6r9pmqj9zb7j1cqk2vRHET9hx1jXlhXDJ6YCwqX6tQSnvFwoRUmXIJbdRF+Jf30yBHFEklKpX0YwxD1M/l7DS/1rpgWjbAL8b0TimrGOXVBwwUIXyC6yqaQlhbonaOOYT4lWIgsypfIFHkZPo8rt2abN9ltT9r4Fzxir4qhGy7qMheXhVITKWBMWQidjm/tK51IAmEEdduqexlS4Mh0iobhX6CqhuC/ij1TS0l1W3H1olOStT2TFgAwT2PG73lxo8KQc0jxsh6N/nHJBWONoIvSaZJCX5JibkR1joQK3uNAGGYRNSNmabW7ByxMNhkoSt4kX2lGOWcBEqT4XgQz96NDaBeAWu43DgcYJ2YZCrB1GwBQjKTMI3KTk6MxR/L6mPUj6VmR7NwoQmMaF0jIoGxL/1lX0cyCfZjKhEIxSJ4zHAE1fIOg5PxDQOhoypT1NITGs3ukbg9/4jxM6+9xuDRtVXtQ09u2eCIkgwnaZeJHqqPRavnxrgOk6RHu4fWgLAfmZ4viWrRnDBUDzHU8XOMRYxRLu0pwtVhnrCh0mgkcEs9ghEYY8+Fiz7srNBIq85oz142yNsRDtUppKMUG6oPociLL+Ymr9BljcyK1KW0oxc+YlbbQN3jhD3MsJ0qd9Tl4kkitxMIin5Q5ocwJraLbY2C7N4Qh+LXw5P9cFWFWrt07efS7xV8OuJiu922zdO+aOLnz+6nYsX3feJNenErWwVyDl0qp7clcnsrilz1M1mamIcSo7NvLEtj8uoQn1Voqk11E/JvG7THxjFR+HDFtb3x1s7DVMK0yWoSKSGpabAQPXIN/bGoZZyncTOuQb/FYvObsPdGPw57/Xog35Hb6M27+0vWtL/L2cFh+UATAFqioFwRmXrusR2AzMtAyxvh8NdLkt0CimjuuAZEwwzvCJrRGiFn1Mql8kBKTLfs4fUjA3td2LL9R3UfQcLfVZWZp8t2UhWL4Rwe6Y4HKKuDnBF6ymT+d44CGlm3ZnHEhanD9r2TXtaALFBr9GxESgwScchU9kF6jG09mGgFxUKxfJmzwtSO/QWmWGsfZvMqPFEQNJ8jGXUj9bClVR2g6ZakqfnqpOOL5968pMvO7r0dlI5Mpt9ijbKbQT7Hfrrvt4daO8V7In56g2tOoS15CCVw4NQL3JHM39hpHfb7x7rRL5CpOR25nf7GZuPDZlkpGEVrQNQ7XdpkNhXnvn+U+QgI21daaPvSxAILEjrb7KUjIPyn9ZDXEngie0nmlxqtdpsF5lxWVlGpi3rpOy4vmpla0Ph03UesWvZxHDW3ltjqBTTxFV6zvb+r6dsit/eE+k0KfaRSmDQe26e9fic5+Ot5xk8MzZT+udFhrPE5j0lPZ1SeIqdBr5mWITAGbPixKVSYW2gicSNong+PHoBkUOOVdBL0BtchaHR656cpabHVxYid/bSAoUu+/sd3aZfebb5Pk7zp7CPRpFdbiJ6cLY+4I9Z84QEhyzQiexo/VDLxn7XtnsY1OTIFR7/w5vGbKC17vrrfMREGMPUdxbLsuqqRNEIn3wfY3dBFvdvyHS/mk5GPj/qBsal/4ds0zfXN9bbd6hUh4l6vKTTZuIuYpGlLcSdD0yh/E5rGu905ze2LsU7fAXGN678sJsJFacQ368+56kb/RP8N1uSepoX37xa4RKnjUqgz2cPVqaZG8bfs8R2Q5cy6HgHpTAKgzGgKisUsZHnh4ShBdxjqNn5FyynOuZhpuls0m1LGwefdnYzPxYNXnX0mDVMu84+Vdn3Xsc8z3ka/fZVY1BZ/SS/HLu/PEbixrk7yKpBxxsVs7DkNDn8pt2teTf11XuB89Yu0CJ1/hJazrq3wBzVii47jQZBAfku8+Glm7Ad85efQ6fQfha2RaIP58rIAAAAASUVORK5CYII=");
+                        break;
+                    case 2:
+                        res = ac.UploadImage(Image.FromFile("02.gif"));
+                        break;
+                    case 3:
+                        ac.is_russian = 1;
+                        res = ac.UploadURL("https://vk.com/captcha.php?s=1&sid=3");
+                        break;
+                    default:
+                        res = ac.UploadFile("01.png");
+                        break;
+                }
+
+                Console.WriteLine(res);
+                Thread.Sleep(2000);
+            }
+
+            //get CAPTCHA ID
+            string id = res.Split(new char[] {'|'})[1];
+
+            string answer = "CAPCHA_NOT_READY";
+
+            //Step 2.waiting for the CAPTCHA recognition
+            while (answer == "CAPCHA_NOT_READY")
+            {
+                Thread.Sleep(3000);
+                answer = ac.GetResult(id);
+                Console.WriteLine(answer);
+            }
+
+            //If OK, CAPTCHA regognized well
+            if (answer.StartsWith("OK|"))
+            {
+                answer = answer.Split(new char[] { '|' })[1];
+                Console.WriteLine("Answer is '{0}'", answer);
+            }
+
+            Console.WriteLine("Press any key...");
+            Console.ReadKey();
+        }
+    }
+}
